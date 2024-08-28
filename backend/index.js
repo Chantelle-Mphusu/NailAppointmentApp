dbbb23c1c4d456f3958520a82b18f74d7580d2a0
Chantelle Monalisa Yaone Mphusu <120548864+Chantelle-Mphusu@users.noreply.github.com>
2024-08-28 11:35:09 +0200

    index

--- backend/index.js
+++ backend/index.js
@@ -26,6 +26,9 @@ app.use(
 );
 
 
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
 
 app.use(bodyParser.json());
 
@@ -35,19 +38,9 @@ app.use(cookieParser());
 app.use(express.json());
 
 
- // Serve static files from the React frontend
- if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'frontend/build')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
-  });
-}
+ 
 
  app.use("/", Route);
 
- app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
 
 
